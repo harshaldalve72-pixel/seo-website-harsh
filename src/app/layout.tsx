f526bb6cdd25669813854828629b4e93b776f265
tejasbabar06 <tejasbabar335@gmail.com>
2026-09-16 13:18:02 +0530

    Fix Vercel build error by removing unused GoogleAnalytics import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 import { Sidebar } from '@/components/layout/Sidebar';
-import { GoogleAnalytics } from '@next/third-parties/google';
+
 
 const outfit = Outfit({ 
   subsets: ['latin'],
